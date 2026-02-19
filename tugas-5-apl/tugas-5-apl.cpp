a0e5ff0d7f9aa86d60aca66c7f2e88ba5008f656
ferlynabil <zenxyny@gmail.com>
2026-02-19 11:17:08 +0800

    Tugas 4,5

---
+++ tugas-5-apl/tugas-5-apl.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i + j < n - 1) {
+                cout << "9 ";
+            } 
+            else if (i + j == n - 1) {
+                cout << "0 ";
+            } 
+            else {
+                cout << "1 ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
